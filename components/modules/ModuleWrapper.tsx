0b13bfdb9f406b2dcb9ba92304b3bb9c05a17ff0
Martin Bearman <martinbearman@hotmail.com>
2025-11-10 09:49:00 +0000

    fix(modules): ensure module wrapper fills available height.

--- components/modules/ModuleWrapper.tsx
+++ components/modules/ModuleWrapper.tsx
@@ -4,7 +4,7 @@ export default function ModuleWrapper({children}: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4 text-black relative w-full">
+    <div className="bg-white rounded-lg shadow-md p-4 text-black relative w-full h-full">
       {children}
     </div>
   );
